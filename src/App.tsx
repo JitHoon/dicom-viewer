7669ee762922d6fccb8607fc2040608ea4194280
Jithoon <imessi0336@gmail.com>
2024-04-05 13:18:09 +0900

    feat: DICOM 이미지 렌더링 및 zoom 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,103 @@
+import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneMath from 'cornerstone-math';
+import * as cornerstoneTools from 'cornerstone-tools';
+import * as cornerstoneWebImageLoader from 'cornerstone-web-image-loader';
+import Hammer from 'hammerjs';
+
 import Header from '@components/Header';
-import Dicom from '@components/Dicom';
+import { createRef, useEffect, useState } from 'react';
 
 function App() {
-  // 배포 테스트
+  const dicomARef = createRef<HTMLDivElement>();
+  const dicomBRef = createRef<HTMLDivElement>();
+  const [currentEl, setCurrentEl] = useState<HTMLDivElement>();
+  const [currentImageA, _setCurrentImageA] = useState<number>(0);
+  const [currentImageB, _setCurrentImageB] = useState<number>(1);
+
+  const imageId_1 =
+    'https://rawgit.com/cornerstonejs/cornerstoneWebImageLoader/master/examples/Renal_Cell_Carcinoma.jpg';
+  const imageId_2 =
+    'https://www.asteris.biz/Keystone/ImageDownload.aspx?ClinicCode=TESTKEYSTONE&ImageId=01b1755e-33d1-4b24-b9af-a4a019689d5f&ImageType=PreviewImage&FrameIndex=0';
+  const imageIds = [imageId_1, imageId_2];
+
+  useEffect(() => {
+    // init cornerstoneTools
+    cornerstoneTools.external.cornerstone = cornerstone;
+    cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+    cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+    cornerstoneTools.external.Hammer = Hammer;
+
+    cornerstoneTools.init();
+
+    const dicomA = dicomARef.current;
+    const dicomB = dicomBRef.current;
+
+    if (dicomA && dicomB) {
+      cornerstone.enable(dicomA);
+      cornerstone.enable(dicomB);
+
+      setCurrentEl(dicomA);
+
+      // add & active ZoomTool
+      cornerstoneTools.addTool(cornerstoneTools.ZoomMouseWheelTool);
+      cornerstoneTools.addTool(cornerstoneTools.ZoomTool);
+
+      cornerstoneTools.setToolActive('ZoomMouseWheel', {
+        mouseButtonMask: 1
+      });
+
+      // display
+      cornerstone.loadAndCacheImage(imageIds[currentImageA]).then(image => {
+        cornerstone.displayImage(dicomA, image);
+      });
+      cornerstone.loadAndCacheImage(imageIds[currentImageB]).then(image => {
+        cornerstone.displayImage(dicomB, image);
+      });
+    } else {
+      alert('이미지를 불러올 수 없습니다.');
+    }
+
+    return () => {
+      if (dicomA && dicomB) {
+        cornerstone.disable(dicomA);
+        cornerstone.disable(dicomB);
+      }
+    };
+  }, []);
+
+  const changeElementBtoA = () =>
+    setCurrentEl(pre => {
+      const dicomA = dicomARef.current;
+      if (dicomA && pre !== dicomA) {
+        return dicomA;
+      } else return pre;
+    });
+
+  const changeElementAtoB = () =>
+    setCurrentEl(pre => {
+      const dicomB = dicomBRef.current;
+      if (dicomB && pre !== dicomB) {
+        return dicomB;
+      } else return pre;
+    });
+
   return (
     <>
-      <Header />
-      <Dicom />
+      <Header currentEl={currentEl} />
+      <main className="box-border flex h-[85vh] w-full items-center justify-center">
+        <div
+          id="A"
+          className="mt-[5px] box-border h-full w-full border-r-[5px] border-solid border-r-[#0F62FE] pr-[5px]"
+          ref={dicomARef}
+          onClick={changeElementBtoA}
+        ></div>
+        <div
+          id="B"
+          className="mt-[5px] box-border h-full w-full border-l-[5px] border-solid border-l-[#0F62FE] pl-[5px]"
+          ref={dicomBRef}
+          onClick={changeElementAtoB}
+        ></div>
+      </main>
     </>
   );
 }
