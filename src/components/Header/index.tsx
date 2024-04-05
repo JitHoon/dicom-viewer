5557ef967f69e3ef863a336ce97472d50199190c
최지훈 <101972330+JitHoon@users.noreply.github.com>
2024-04-05 13:32:41 +0900

    [#3] 마크업 수정 및 DICOM 이미지 생성하기 (#4)
    * design: cornerstonejs 적용을 위한 일부 스타일 수정
    
    * init: cornerstonejs 및 확장 라이브러리 설치
    
    * feat: DICOM 이미지 렌더링 및 zoom 기능
    
    * fix: import module type 에러로 인한 .d.ts 파일 정의

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,36 @@
-import FeatureBar from './FeatureBar';
+import * as cornerstoneTools from 'cornerstone-tools';
 
-const Header = () => {
+const Header = ({ currentEl }: { currentEl: HTMLDivElement | undefined }) => {
+  const zoomHandler = () => {
+    cornerstoneTools.setToolActiveForElement(currentEl, 'ZoomMouseWheel', {
+      mouseButtonMask: 1
+    });
+  };
+
   return (
-    <header className="flex h-[116px] w-full items-center justify-between gap-12 border-b-[5px] border-solid border-b-[#0F62FE] py-4 pl-[30px] pr-10">
-      <h1 className="whitespace-nowrap text-left text-xl font-bold leading-[22px]">
+    <header className="box-border flex h-[116px] w-full items-center justify-between gap-12 border-b-[5px] border-solid border-b-[#0F62FE] px-[30px] py-[47px]">
+      <h1 className="whitespace-nowrap text-left text-xl font-bold leading-[10px]">
         Dicom Viewer (with Cornerstone.js)
       </h1>
-      <FeatureBar />
+      <div className="flex items-center justify-between gap-6">
+        <div className="flex items-center justify-between gap-4">
+          <button className="feat-btn" onClick={zoomHandler}>
+            Zoom
+          </button>
+          <button className="feat-btn">Flip H</button>
+          <button className="feat-btn">Filp V</button>
+          <button className="feat-btn">Rotate Delta 30</button>
+          <button className="feat-btn">Invert</button>
+          <button className="feat-btn">Apply Colormap</button>
+          <button className="feat-btn">Reset</button>
+        </div>
+        <button className="img-btn">
+          <span className="px-2">Previous Image</span>
+        </button>
+        <button className="img-btn">
+          <span className="px-2">Next Image</span>
+        </button>
+      </div>
     </header>
   );
 };
